2e16e1a31e2e0768b2a3c0d0c83caa31a05fc67e
Ricardosz <948573412@qq.com>
2017-05-29 16:40:40 +0800

    1.0
    1.0

---
+++ FTP/FTP/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace FTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbxloginmessage = new System.Windows.Forms.TextBox();
+            this.btnlogout = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.chkbxAnonymous = new System.Windows.Forms.CheckBox();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxServerIp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstbxFtpState = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btndownload = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.lstbxFtpResources = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.tbxloginmessage);
+            this.groupBox1.Controls.Add(this.btnlogout);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.btnlogin);
+            this.groupBox1.Controls.Add(this.chkbxAnonymous);
+            this.groupBox1.Controls.Add(this.tbxPassword);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbxUsername);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbxServerIp);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 12F);
+            this.groupBox1.Location = new System.Drawing.Point(33, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(279, 281);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "服务器信息";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 12F);
+            this.label5.Location = new System.Drawing.Point(24, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "登录信息：";
+            // 
+            // tbxloginmessage
+            // 
+            this.tbxloginmessage.Font = new System.Drawing.Font("宋体", 12F);
+            this.tbxloginmessage.Location = new System.Drawing.Point(24, 240);
+            this.tbxloginmessage.Name = "tbxloginmessage";
+            this.tbxloginmessage.Size = new System.Drawing.Size(218, 26);
+            this.tbxloginmessage.TabIndex = 10;
+            // 
+            // btnlogout
+            // 
+            this.btnlogout.Font = new System.Drawing.Font("宋体", 12F);
+            this.btnlogout.Location = new System.Drawing.Point(146, 171);
+            this.btnlogout.Name = "btnlogout";
+            this.btnlogout.Size = new System.Drawing.Size(96, 23);
+            this.btnlogout.TabIndex = 9;
+            this.btnlogout.Text = "注销";
+            this.btnlogout.UseVisualStyleBackColor = true;
+            this.btnlogout.Click += new System.EventHandler(this.btnlogout_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 12F);
+            this.label4.Location = new System.Drawing.Point(183, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "匿名";
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Font = new System.Drawing.Font("宋体", 12F);
+            this.btnlogin.Location = new System.Drawing.Point(22, 171);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(84, 23);
+            this.btnlogin.TabIndex = 8;
+            this.btnlogin.Text = "登录";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // chkbxAnonymous
+            // 
+            this.chkbxAnonymous.AutoSize = true;
+            this.chkbxAnonymous.Font = new System.Drawing.Font("宋体", 12F);
+            this.chkbxAnonymous.Location = new System.Drawing.Point(227, 88);
+            this.chkbxAnonymous.Name = "chkbxAnonymous";
+            this.chkbxAnonymous.Size = new System.Drawing.Size(15, 14);
+            this.chkbxAnonymous.TabIndex = 7;
+            this.chkbxAnonymous.UseVisualStyleBackColor = true;
+            this.chkbxAnonymous.Click += new System.EventHandler(this.chkbxAnonymous_Click);
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.Font = new System.Drawing.Font("宋体", 12F);
+            this.tbxPassword.Location = new System.Drawing.Point(114, 122);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.Size = new System.Drawing.Size(128, 26);
+            this.tbxPassword.TabIndex = 5;
+            this.tbxPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxPassword_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F);
+            this.label3.Location = new System.Drawing.Point(22, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "密码";
+            // 
+            // tbxUsername
+            // 
+            this.tbxUsername.Font = new System.Drawing.Font("宋体", 12F);
+            this.tbxUsername.Location = new System.Drawing.Point(114, 78);
+            this.tbxUsername.Name = "tbxUsername";
+            this.tbxUsername.Size = new System.Drawing.Size(63, 26);
+            this.tbxUsername.TabIndex = 3;
+            this.tbxUsername.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxUsername_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F);
+            this.label2.Location = new System.Drawing.Point(22, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "用户名";
+            // 
+            // tbxServerIp
+            // 
+            this.tbxServerIp.Font = new System.Drawing.Font("宋体", 12F);
+            this.tbxServerIp.Location = new System.Drawing.Point(114, 32);
+            this.tbxServerIp.Name = "tbxServerIp";
+            this.tbxServerIp.Size = new System.Drawing.Size(128, 26);
+            this.tbxServerIp.TabIndex = 1;
+            this.tbxServerIp.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxServerIp_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F);
+            this.label1.Location = new System.Drawing.Point(19, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "服务器地址";
+            // 
+            // lstbxFtpState
+            // 
+            this.lstbxFtpState.Font = new System.Drawing.Font("宋体", 12F);
+            this.lstbxFtpState.FormattingEnabled = true;
+            this.lstbxFtpState.ItemHeight = 16;
+            this.lstbxFtpState.Location = new System.Drawing.Point(6, 35);
+            this.lstbxFtpState.Name = "lstbxFtpState";
+            this.lstbxFtpState.Size = new System.Drawing.Size(798, 116);
+            this.lstbxFtpState.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstbxFtpState);
+            this.groupBox2.Font = new System.Drawing.Font("宋体", 12F);
+            this.groupBox2.Location = new System.Drawing.Point(33, 350);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(824, 167);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "连接信息：";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnDelete);
+            this.groupBox3.Controls.Add(this.btndownload);
+            this.groupBox3.Controls.Add(this.btnUpload);
+            this.groupBox3.Controls.Add(this.lstbxFtpResources);
+            this.groupBox3.Font = new System.Drawing.Font("宋体", 12F);
+            this.groupBox3.Location = new System.Drawing.Point(347, 41);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(510, 281);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "文件列表：";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("宋体", 12F);
+            this.btnDelete.Location = new System.Drawing.Point(415, 252);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "删除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btndownload
+            // 
+            this.btndownload.Font = new System.Drawing.Font("宋体", 12F);
+            this.btndownload.Location = new System.Drawing.Point(122, 252);
+            this.btndownload.Name = "btndownload";
+            this.btndownload.Size = new System.Drawing.Size(75, 23);
+            this.btndownload.TabIndex = 2;
+            this.btndownload.Text = "下载";
+            this.btndownload.UseVisualStyleBackColor = true;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Font = new System.Drawing.Font("宋体", 12F);
+            this.btnUpload.Location = new System.Drawing.Point(16, 252);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 1;
+            this.btnUpload.Text = "上传";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // lstbxFtpResources
+            // 
+            this.lstbxFtpResources.Font = new System.Drawing.Font("宋体", 12F);
+            this.lstbxFtpResources.FormattingEnabled = true;
+            this.lstbxFtpResources.ItemHeight = 16;
+            this.lstbxFtpResources.Location = new System.Drawing.Point(16, 20);
+            this.lstbxFtpResources.Name = "lstbxFtpResources";
+            this.lstbxFtpResources.Size = new System.Drawing.Size(474, 212);
+            this.lstbxFtpResources.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 539);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "FTP";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbxloginmessage;
+        private System.Windows.Forms.Button btnlogout;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.CheckBox chkbxAnonymous;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxServerIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstbxFtpState;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btndownload;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.ListBox lstbxFtpResources;
+    }
+}
+
---
+++ FTP/FTP/Form1.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+//添加所需的命名空间
+using System.Net;
+using System.IO;
+
+namespace FTP
+{
+    public partial class Form1 : Form
+    {   //默认FTP端口为21
+        private const int ftpport = 21;
+        private string ftpUristring = null;
+        private NetworkCredential networkCredential;
+        private string currentDir = "/";
+
+        public Form1()
+        {
+            InitializeComponent();
+            IPAddress[] ips = Dns.GetHostAddresses("");
+            tbxServerIp.Text = ips[1].ToString();
+            tbxServerIp.SelectAll();
+            btndownload.Enabled = false;
+            lstbxFtpResources.Enabled = false;
+            lstbxFtpState.Enabled = false;
+            btnUpload.Enabled = false;
+            btndownload.Enabled = false;
+            btnDelete.Enabled = false;
+        }
+
+
+        //获取键盘操作
+        private void tbxServerIp_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //输入服务器IP后，焦点跳转到用户名文本框
+            if (e.KeyChar == (char)13)
+            {
+                tbxUsername.Focus();
+            }
+        }
+
+        private void tbxUsername_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //输入用户名后，焦点跳转到密码文本框
+            if (e.KeyChar == (char)13)
+            {
+                tbxPassword.Focus();
+            }
+        }
+
+        private void tbxPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //输入密码后，焦点跳转到登陆按钮
+            if (e.KeyChar == (char)13)
+            {
+                btnlogin.Focus();
+            }
+
+        }
+        //匿名事件
+        private void chkbxAnonymous_Click(object sender, EventArgs e)
+        {
+            //实名登录
+            if (chkbxAnonymous.Checked == false)
+            {
+                tbxUsername.Enabled = true;
+                tbxUsername.Text = "user";
+                tbxPassword.Enabled = true;
+                tbxPassword.Text = "";
+                tbxUsername.Focus();
+            }
+            //匿名登录
+            else
+            {
+                tbxUsername.Text = "anonymous";
+                tbxUsername.Enabled = false;
+                tbxPassword.Text = "";
+                tbxPassword.Enabled = false;
+            }
+        }
+        //服务器交互
+        //创建FTP连接
+        private FtpWebRequest CreateFtpWebRequest(string uri, string requestMethod)
+        {
+            FtpWebRequest request = (FtpWebRequest)FtpWebRequest.Create(uri);
+            request.Credentials = networkCredential;
+            request.KeepAlive = true;
+            request.UseBinary = true;
+            request.Method = requestMethod;
+            return request;
+        }
+        //获取服务器返回信息
+        private FtpWebResponse GetFtpResponse(FtpWebRequest request)
+        {
+            FtpWebResponse response = null;
+            try
+            {
+                response = (FtpWebResponse)request.GetResponse();
+                lstbxFtpState.Items.Add("验证完毕，服务器返回信息：[" + response.WelcomeMessage + "]");
+                lstbxFtpState.Items.Add("正在连接：[" + response.BannerMessage + "]");
+                lstbxFtpState.TopIndex = lstbxFtpState.Items.Count - 1;
+                return response;
+            }
+            catch (WebException ex)
+            {
+                lstbxFtpState.Items.Add("发送错误。返回信息为：" + ex.Status);
+                lstbxFtpState.TopIndex = lstbxFtpState.Items.Count - 1;
+                return null;
+            }
+        }
+        //登录模块的实现
+        //登录服务器事件
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            if (tbxServerIp.Text == string.Empty)
+            {
+                MessageBox.Show("请先填写服务器地址", "提示");
+                return;
+            }
+            ftpUristring = "ftp://" + tbxServerIp.Text;
+            networkCredential = new NetworkCredential(tbxUsername.Text, tbxPassword.Text);
+            if (ShowFtpFileAndDirectory() == true)
+            {
+                btnlogin.Enabled = false;
+                btnlogout.Enabled = true;
+                lstbxFtpResources.Enabled = true;
+                lstbxFtpState.Enabled = true;
+                tbxServerIp.Enabled = false;
+                if (chkbxAnonymous.Checked == false)
+                {
+                    tbxUsername.Enabled = false;
+                    tbxPassword.Enabled = false;
+                    chkbxAnonymous.Enabled = false;
+                }
+                else
+                {
+                    chkbxAnonymous.Enabled = false;
+                }
+                tbxloginmessage.Text = "登录成功";
+                btnUpload.Enabled = true;
+                btndownload.Enabled = true;
+                btnDelete.Enabled = true;
+            }
+            else
+            {
+                lstbxFtpState.Enabled = true;
+                tbxloginmessage.Text = "登录失败";
+            }
+        }
+
+        // 显示资源列表
+        private bool ShowFtpFileAndDirectory()
+        {
+            lstbxFtpResources.Items.Clear();
+            string uri = string.Empty;
+            if (currentDir == "/")
+            {
+                uri = ftpUristring;
+            }
+            else
+            {
+                uri = ftpUristring + currentDir;
+            }
+
+            string[] urifield = uri.Split(' ');
+            uri = urifield[0];
+            FtpWebRequest request = CreateFtpWebRequest(uri, WebRequestMethods.Ftp.ListDirectoryDetails);
+
+            // 获得服务器返回的响应信息
+            FtpWebResponse response = GetFtpResponse(request);
+            if (response == null)
+            {
+                return false;
+            }
+            lstbxFtpState.Items.Add("连接成功，服务器返回的是：" + response.StatusCode + " " + response.StatusDescription);
+
+            // 读取网络流数据
+            Stream stream = response.GetResponseStream();
+            StreamReader streamReader = new StreamReader(stream, Encoding.Default);
+            lstbxFtpState.Items.Add("获取响应流....");
+            string s = streamReader.ReadToEnd();
+            streamReader.Close();
+            stream.Close();
+            response.Close();
+            lstbxFtpState.Items.Add("传输完成");
+
+            // 处理并显示文件目录列表
+            string[] ftpdir = s.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            lstbxFtpResources.Items.Add("↑返回上层目录");
+            int length = 0;
+            for (int i = 0; i < ftpdir.Length; i++)
+            {
+                if (ftpdir[i].EndsWith("."))
+                {
+                    length = ftpdir[i].Length - 2;
+                    break;
+                }
+            }
+
+            for (int i = 0; i < ftpdir.Length; i++)
+            {
+                s = ftpdir[i];
+                int index = s.LastIndexOf('\t');
+                if (index == -1)
+                {
+                    if (length < s.Length)
+                    {
+                        index = length;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                string name = s.Substring(index + 1);
+                if (name == "." || name == "..")
+                {
+                    continue;
+                }
+
+                // 判断是否为目录，在名称前加"目录"来表示
+                if (s[0] == 'd' || (s.ToLower()).Contains("<dir>"))
+                {
+                    string[] namefield = name.Split(' ');
+                    int namefieldlength = namefield.Length;
+                    string dirname;
+                    dirname = namefield[namefieldlength - 1];
+
+                    // 对齐
+                    dirname = dirname.PadRight(34, ' ');
+                    name = dirname;
+                    // 显示目录
+                    lstbxFtpResources.Items.Add("[目录]" + name);
+                }
+            }
+
+            for (int i = 0; i < ftpdir.Length; i++)
+            {
+                s = ftpdir[i];
+                int index = s.LastIndexOf('\t');
+                if (index == -1)
+                {
+                    if (length < s.Length)
+                    {
+                        index = length;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                string name = s.Substring(index + 1);
+                if (name == "." || name == "..")
+                {
+                    continue;
+                }
+
+                // 判断是否为文件
+                if (!(s[0] == 'd' || (s.ToLower()).Contains("<dir>")))
+                {
+                    string[] namefield = name.Split(' ');
+                    int namefieldlength = namefield.Length;
+                    string filename;
+
+                    filename = namefield[namefieldlength - 1];
+
+                    // 对齐
+                    filename = filename.PadRight(34, ' ');
+                    name = filename;
+
+                    // 显示文件
+                    lstbxFtpResources.Items.Add(name);
+                }
+            }
+
+            return true;
+        }
+
+        //注销事件
+        private void btnlogout_Click(object sender, EventArgs e)
+        {
+            btnlogin.Enabled = true;
+            btnlogout.Enabled = false;
+            tbxServerIp.Enabled = true;
+            tbxServerIp.SelectAll();
+            tbxServerIp.Focus();
+            chkbxAnonymous.Enabled = true;
+            if (chkbxAnonymous.Checked == false)
+            {
+                tbxUsername.Enabled = true;
+                tbxPassword.Enabled = true;
+            }
+            tbxloginmessage.Text = "你已经退出";
+            lstbxFtpResources.Items.Clear();
+            lstbxFtpResources.Enabled = false;
+            lstbxFtpState.Items.Clear();
+            lstbxFtpState.Enabled = false;
+            btnUpload.Enabled = false;
+            btndownload.Enabled = false;
+            btnDelete.Enabled = false;
+        }
+
+
+        //文件操作模块的实现
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            //选择要上传的文件
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.FileName = openFileDialog.FileName.ToString();
+            openFileDialog.Filter = "所有文件(*.*)|*.*";
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            FileInfo fileinfo = new FileInfo(openFileDialog.FileName);
+            try
+            {
+                string uri = GetUriString(fileinfo.Name);
+                FtpWebRequest request = CreateFtpWebRequest(uri, WebRequestMethods.Ftp.UploadFile);
+                request.ContentLength = fileinfo.Length;
+                int buflength = 8196;
+                byte[] buffer = new byte[buflength];
+                FileStream filestream = fileinfo.OpenRead();
+                Stream responseStream = request.GetRequestStream();
+                lstbxFtpState.Items.Add("打开上传流，文件上传中...");
+                int contenlength = filestream.Read(buffer, 0, buflength);
+                while (contenlength != 0)
+                {
+                    responseStream.Write(buffer, 0, contenlength);
+                    contenlength = filestream.Read(buffer, 0, buflength);
+                }
+                responseStream.Close();
+                filestream.Close();
+                FtpWebResponse response = GetFtpResponse(request);
+                if (response == null)
+                {
+                    lstbxFtpState.Items.Add("服务器未响应...");
+                    lstbxFtpState.TopIndex = lstbxFtpState.Items.Count - 1;
+                    return;
+                }
+                lstbxFtpState.Items.Add("上传完毕，服务器返回:" + response.StatusCode + "" + response.StatusDescription);
+                lstbxFtpState.TopIndex = lstbxFtpState.Items.Count - 1;
+                MessageBox.Show("上传成功");
+                //上传成功后，即时刷新服务器目录
+                ShowFtpFileAndDirectory();
+            }
+            catch (WebException ex)
+            {
+                lstbxFtpState.Items.Add("上传失败，返回信息为:" + ex.Status);
+                lstbxFtpState.TopIndex = lstbxFtpState.Items.Count - 1;
+                MessageBox.Show(ex.Message, "上传失败");
+            }
+
+        }
+
+
+        private string GetUriString(string filename)
+        {
+            string uri = string.Empty;
+            if (currentDir.EndsWith("/"))
+            {
+                uri = ftpUristring + currentDir + filename;
+            }
+            else
+            {
+                uri = ftpUristring + currentDir + "/" + filename;
+            }
+            return uri;
+        }
+        //从服务器上下载文件到本地
+
+
+
+
+
+
+    }
+
+    }
+
+
